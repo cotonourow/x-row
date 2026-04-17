b8acfd290ab2cf581b4e425e5eaaa9059019dbeb
x-row <cotonourow@gmail.com>
2026-04-17 15:09:45 +0100

    add homepage route

--- cotonourow/urls.py
+++ cotonourow/urls.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("X-ROW Backend is LIVE 🚀")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('contacts/', include('contacts.urls')),
+    path('', home),  # 👈 ADD THIS LINE
 ]
